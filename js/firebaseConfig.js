1de8c5c877dbfc30a4663130c369336a6eeb320b
Dhi15 <89863092+Dhi15@users.noreply.github.com>
2023-02-05 15:18:48 +0530

    Add files via upload

---
+++ js/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+  var config = {
+    apiKey: "AIzaSyDnBb2naXVZPC2oKxLVlMhMTgG4F6bEjb4",
+    authDomain: "shopdemo-52115.firebaseapp.com",
+    projectId: "shopdemo-52115",
+    databaseURL: "https://shopdemo-52115-default-rtdb.firebaseio.com",
+    storageBucket: "shopdemo-52115.appspot.com",
+    messagingSenderId: "1037415744989",
+    appId: "1:1037415744989:web:69ba1847bf3911fee72234"
+    };
+    firebase.initializeApp(config);
+  
